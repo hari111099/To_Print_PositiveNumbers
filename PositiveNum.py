afa8235ff4fc14bfd2b7c120124dbd805a33d050
hari111099 <87607273+hari111099@users.noreply.github.com>
2021-07-18 19:10:33 +0530

    PositiveNum.py
    using python

---
+++ PositiveNum.py
@@ -0,0 +1,10 @@
+list1=[1,-9,56,-63,-7,-54,2,4,79,78,-6,5,-6,-4]
+list2=[2,-7,6,4,-33,74,-99,-54,21,35,99,-78,-66]
+print("positive numbers in list1")
+for i in list1:
+       if(i>0):
+          print(i)
+print("positive numbers in list2")
+for n in list2:
+       if(n>0):
+          print(n)
